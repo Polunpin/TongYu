5513553d70c8a0582bb78c11e182fc0b7bc495c3
lanyiping <lanyip@outlook.com>
2024-10-19 12:17:53 +0800

    jsCode2session-4｜部署重试

--- src/main/java/com/TongYu/TongYuRunApplication.java
+++ src/main/java/com/TongYu/TongYuRunApplication.java
@@ -1,6 +1,5 @@
 package com.TongYu;
 
-import com.TongYu.controller.WeComController;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
