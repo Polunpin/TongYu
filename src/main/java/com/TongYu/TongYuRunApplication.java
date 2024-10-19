36a4e6e54920469d7d85365b5fcd37e819a75385
lanyiping <lanyip@outlook.com>
2024-10-19 17:44:33 +0800

    回退排查问题-003

--- src/main/java/com/TongYu/TongYuRunApplication.java
+++ src/main/java/com/TongYu/TongYuRunApplication.java
@@ -1,9 +1,9 @@
 package com.TongYu;
 
+import com.TongYu.controller.WeComController;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -11,8 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan(basePackages = {"com.TongYu.mapper"})
 public class TongYuRunApplication {
 
-  public static void main(String[] args) throws JSONException {
+  public static void main(String[] args) {
     SpringApplication.run(TongYuRunApplication.class, args);
-//    new WeComController().getCorpAccessToken();
+    new WeComController().getCorpAccessToken();
   }
 }
