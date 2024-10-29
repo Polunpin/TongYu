015fa4a8c6ce22493d253797d5ef057c3076a594
lanyiping <lanyip@outlook.com>
2024-10-29 18:14:02 +0800

    自测.03-预约课程信息

--- src/main/java/com/TongYu/service/impl/LessonManagementServiceImpl.java
+++ src/main/java/com/TongYu/service/impl/LessonManagementServiceImpl.java
@@ -73,7 +73,7 @@ public class LessonManagementServiceImpl implements LessonManagementService {
     public Boolean reservation(CourseAddRequest courseAddRequest) {
         Student student = new Student();
         log.info("当前预约课程信息:{}", courseAddRequest);
-        if (StringUtils.isBlank(courseAddRequest.getImageId())) {
+        if (StringUtils.isNotBlank(courseAddRequest.getImageId())) {
             //体验课
             student.setStuName(courseAddRequest.getStuName());
             student.setOpenId(courseAddRequest.getOpenId());
